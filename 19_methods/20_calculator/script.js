19e6d986399f2b95256903a472c13f29753c9dc1
ashok <urashok15@gmail.com>
2025-07-02 18:31:11 +0530

    calculate

---
+++ 19_methods/20_calculator/script.js
@@ -0,0 +1,21 @@
+
+let operator = prompt("Choose a@addition, b@subtraction , c@multiplication ,d@division");
+let num1 = parseInt(prompt("Enter firstNumber"));
+let num2 = parseInt(prompt("Enter secondNumber"));
+let result;
+
+if (operator === 'a') {
+    result = num1 + num2;
+}
+else if (operator === "b") {
+    result = num1 - num2;
+}
+else if (operator === "c") {
+    result = num1 * num2;
+}
+else {
+    result = num1 / num2;
+}
+
+
+alert("The result is : " + result);
